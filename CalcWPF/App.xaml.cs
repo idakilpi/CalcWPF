b17ab6c83ac8affca2423ac849efd571f4a88aab
Ida Yliluoma <Ida.Yliluoma@student.careeria.fi>
2024-01-19 23:23:22 +0200

    Add project files.

---
+++ CalcWPF/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace CalcWPF
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ CalcWPF/MainWindow.xaml.cs
@@ -0,0 +1,110 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace CalcWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        int first;
+        int second;
+
+        char op;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Button btn = (Button)sender;
+
+            if (TxtResult.Text.Length < 1 && int.TryParse(btn.Content.ToString(), out int newDigit))
+            {
+                if (newDigit >= 1 && newDigit <= 9)
+                {
+                    TxtResult.Text += newDigit.ToString();
+                    second = newDigit;
+                }
+            }
+        }
+
+
+        private void Divide_Click(object sender, RoutedEventArgs e)
+        {
+            first = Int32.Parse(TxtResult.Text);
+            op = '/';
+            TxtResult.Clear();
+        }
+
+        private void MulButton_Click(object sender, RoutedEventArgs e)
+        {
+            first = Int32.Parse(TxtResult.Text);
+            op = '*';
+            TxtResult.Clear();
+        }
+
+        private void subButton_Click(object sender, RoutedEventArgs e)
+        {
+            first = Int32.Parse(TxtResult.Text);
+            op = '-';
+            TxtResult.Clear();
+        }
+
+        private void AddButton_Click(object sender, RoutedEventArgs e)
+        {
+            first = Int32.Parse(TxtResult.Text);
+            op = '+';
+            TxtResult.Clear();
+        }
+
+        private void Equals_Click(object sender, RoutedEventArgs e)
+        {
+            second = Int32.Parse(TxtResult.Text);
+            int result = 0;
+
+
+            if (op == '+')
+            {
+                result = first + second;
+            }
+            else if (op == '-')
+            {
+                result = first - second;
+            }
+            else if (op == '*')
+            {
+                result = first * second;
+            }
+            else if (op == '/')
+            {
+                result = first / second;
+            }
+            if (TxtResult.Text == "0")
+            {
+                TxtResult.Clear();
+            }
+            TxtResult.Text = result.ToString();
+        }
+
+
+        private void Clear_Click(object sender, RoutedEventArgs e)
+        {
+            first = 0;
+            second = 0;
+            op = '\0';
+            TxtResult.Clear();
+        }
+    }
+}
